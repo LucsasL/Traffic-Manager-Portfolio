a7d803a75e124da51254ce41cd5438fe29f930ed
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-19 21:34:06 -0300

    Starting to add icons and menus for mobile

---
+++ script/menu.js
@@ -0,0 +1,23 @@
+const navMenu = document.querySelector("menu");
+const menuButton = document.querySelector("div#menuButton")
+
+function menu() {
+  if (navMenu.style.display == "none") {
+    navMenu.style.display = "block";
+
+  } else {
+    navMenu.style.display = "none";
+
+  }
+}
+
+function menuBehavior() {
+  if (window.innerWidth <= 768) {
+    navMenu.style.display = "none";
+    menuButton.style.display = "block";
+
+  } else {
+    navMenu.style.display = "block";
+
+  }
+}
