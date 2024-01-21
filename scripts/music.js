1a16a7112ef3ae39c2eb5891c3d59555340779cb
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-21 12:56:34 -0300

    Tried to us some audio

--- scripts/music.js
+++ scripts/music.js
@@ -1,11 +1,15 @@
 const musicButton = document.querySelector("input#music");
+const audioPlayer = document.querySelector("audio#player");
 
-function musicControl() {
-  if (musicButton.addEventListener) {
+function musicControl(musicName) {
+  let music = new Audio(musicName);
+  music.loop = true;
+  music.controls;
 
+  musicButton.addEventListener("click", function () {
+    if (musicButton.checked == false) {
+      music.pause();
 
-  } else {
-
-    
-  }
+    }
+  })
 }
