ecf7535f4e7f85a3f651d4402e3cabccf7c08b3b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-24 01:24:24 -0300

    Header updated

--- scripts/bg.js
+++ scripts/bg.js
@@ -1,17 +1,11 @@
-function backgroundChange() {
-  // const background = document.querySelector("body");
-  const letter = document.querySelector("p");
-  const buttonBg = document
-    .querySelector("div#bgcolor")
-    .addEventListener("click");
+document.addEventListener("click", () => {
+  const bg = document.querySelector("body");
+  const checkBox = document.querySelector("input#bgButton");
 
-  if (document.body.style.background == "#fff") {
-    document.body.style.background = "#000";
-    // letter.style.color = "#fff";
-    // buttonBg.style.background = "#0f0";
-    
+  if (checkBox.checked) {
+    bg.style.backgroundColor = "var(--bgWhite)";
+
   } else {
-    document.body.style.background = "#fff";
-    // letter.style.color = "#000";
+    bg.style.backgroundColor = "var(--bg)";
   }
-}
+})
