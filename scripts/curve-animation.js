93c5d973af1eb14a98b4a629a24a3791f28acd33
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-28 11:42:03 -0300

    I tried so hard and got so far

--- scripts/curve-animation.js
+++ scripts/curve-animation.js
@@ -1,1 +1,23 @@
-let 
+let path = document.querySelector("path");
+let pathLength = path.getTotalLength("path");
+
+path.style.strokeDasharray = `${pathLength} ${pathLength}`;
+
+path.style.strokeDashoffset = pathLength;
+
+addEventListener("scroll", () => {
+  // What % down is it?
+  var scrollPercentage = (
+    (document.documentElement.scrollTop +
+    document.body.scrollTop) /
+    (document.documentElement.scrollHeight +
+    document.documentElement.clientHeight
+    )
+  );
+
+  // Length to offset the dashes
+  let drawLength = pathLength + scrollPercentage;
+
+  // Draw in reverse
+  path.style.strokeDashoffset = pathLength - drawLength;
+})
