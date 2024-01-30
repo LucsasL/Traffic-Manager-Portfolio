34b54d0d8a46ab8d9a8f7ca13d9282a480aa584b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-01-30 17:32:37 -0300

    Blocks animation done

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,15 +1,11 @@
 const cursor = document.querySelector("div#cursorCircle");
-const blocks = document.querySelector("div.promises");
 
 window.addEventListener("mousemove", function (e) {
 
   const posX = e.clientX;
   const posY = e.clientY;
 
+  /* Adding the position to the cursor */
   cursor.style.left = `${posX}px`;
   cursor.style.top = `${posY}px`;
-
-  if (posX == blocks.clientX && posY == blocks.clientX) {
-    e.style.display = "block";
-  }
 })
